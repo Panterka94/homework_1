7f0e6a8b49e04664115f91b2ef8d57e8d48e667a
Lena <lena4ka94@inbox.ru>
2018-09-16 20:39:47 +0300

    завершина первая задача

---
+++ ideal_weight.rb
@@ -0,0 +1,39 @@
+# Программа запрашивает у пользователя имя и рост и выводит идеальный вес по формуле <рост> - 110, после чего
+# выводит результат пользователю на экран с обращением по имени. Если идеальный вес получается отрицательным,
+# то выводится строка "Ваш вес уже оптимальный"
+
+
+# Запрашиваем имя рост
+# Считаем вес по формуле
+# Выводим результат
+
+
+def ask_name
+  puts "please, write your name: "
+  name = gets.strip
+  return name
+end
+
+def ask_height
+  puts "please, write your height: "
+  height = gets.to_i
+  return height
+end
+
+def calculate_weight(height)
+  ideal = height - 110
+  return ideal
+end
+
+
+user_name = ask_name
+user_height = ask_height
+user_ideal_weight = calculate_weight(user_height)
+
+
+if user_ideal_weight > 0
+  puts "#{user_name} your ideal weight is #{user_ideal_weight}"
+else
+  puts "#{user_name} your weight is ideal"
+end
+
